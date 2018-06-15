0f9bda0a181defa4dcb87706f8bdbfbcf7331b54
Jack <lehoangan1988@gmail.com>
2018-06-15 12:21:28 +0700

    Help install all Vietnam HR feature

---
+++ hr_vietnam/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# Copyright 2009-2018 Trobz (http://trobz.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Vietnam HR",
+    'version': '11.0.1.0.1',
+    "author": "Trobz, Odoo Community Association (OCA)",
+    "website": "http://trobz.com",
+    'license': 'AGPL-3',
+    'category': 'Human Resources',
+    "depends": [
+        "hr_employee_vn",
+        "hr_document_vn",
+        "hr_contract_vn",
+        "hr_payroll_vn",
+        "hr_payroll_advance_vn",
+        "hr_recruitment_vn",
+        "hr_employee_vn",
+        "l10n_vn_hr_payroll",
+        "hr_appraisal",
+        "hr_attendance_extension",
+        "hr_holidays_extension",
+        "hr_mail_contract_end",
+        "hr_mail_holiday",
+        "hr_overtime",
+        "hr_public_holiday",
+        "hr_working_hour_vn",
+    ],
+    'data': [
+    ],
+    'installable': True,
+}
